23a605d5db162e810fbaac6059f2f49d147eb069
Łukasz Kalita <lukkalita@onet.pl>
2025-01-10 15:39:24 +0100

    build for shield

--- src/main/java/org/example/equipment/shields/Shield.java
+++ src/main/java/org/example/equipment/shields/Shield.java
@@ -1,4 +1,15 @@
 package org.example.equipment.shields;
 
+import org.example.enums.Stealth;
+import org.example.equipment.effects.ArmorEffect;
+
+import java.util.List;
+
 public class Shield {
+    String name;
+    int blockModification;
+    int blockScore;
+    Stealth stealth;
+    int value;
+    List <ArmorEffect> effectList;
 }
