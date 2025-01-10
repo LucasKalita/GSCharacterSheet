573f9d5af0569b67f904ca7a657d32900dae034e
Łukasz Kalita <lukkalita@onet.pl>
2025-01-10 15:20:49 +0100

    cc weapons build book friendly

---
+++ src/main/java/org/example/equipment/weapons/Weapon.java
@@ -0,0 +1,17 @@
+package org.example.equipment.weapons;
+
+import org.example.equipment.effects.WeaponEffect;
+
+import java.util.List;
+import java.util.Set;
+
+public class Weapon {
+    String name;
+    String use;
+    String range;
+    int hitModification;
+    int power;
+    int value;
+    Set<WeaponEffect> weaponEffects;
+    String description;
+}
