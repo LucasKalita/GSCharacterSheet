dc04720e2487f76e65576a588fb948186f91fd33
Łukasz Kalita <lukkalita@onet.pl>
2025-01-10 15:28:50 +0100

    base for skill build

--- src/main/java/org/example/skills/Skill.java
+++ src/main/java/org/example/skills/Skill.java
@@ -1,4 +1,11 @@
 package org.example.skills;
 
+import org.example.enums.SkillMasteryGrade;
+
+import java.util.Map;
+
 public class Skill {
+    String name;
+    String description;
+    Map<SkillMasteryGrade, Integer> masteryAndBonusLevel;
 }
