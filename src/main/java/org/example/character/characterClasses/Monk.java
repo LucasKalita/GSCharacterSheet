7c10568d4a950c3e2d6046fb37004dedde2d88b3
Łukasz Kalita <lukkalita@onet.pl>
2025-01-13 18:33:30 +0100

    Class for Monk

--- src/main/java/org/example/character/characterClasses/Monk.java
+++ src/main/java/org/example/character/characterClasses/Monk.java
@@ -8,5 +8,25 @@ import org.example.enums.TypeOfWeapons;
 import java.util.List;
 
 public class Monk extends CharacterClass {
+    public Monk(String name,
+                boolean isCore,
+                List<TypeOfWeapons> allowedWeaponList,
+                List<TypeOfArmor> allowedArmorList,
+                List<Checks> allowedChecks,
+                int level,
+                SpellCastSystem spellCastSystem) {
+        super("Monk",
+                true,
+                List.of(TypeOfWeapons.CLOSE_COMBAT,
+                        TypeOfWeapons.THROWING,
+                        TypeOfWeapons.STAFF),
+                List.of(TypeOfArmor.CLOTH),
+                List.of(Checks.DODGE,
+                        Checks.HIT,
+                        Checks.ACROBATICS,
+                        Checks.JUMP),
+                1,
+                SpellCastSystem.NONE);
 
+    }
 }
