a515226d21e07e5f999a76de4509fda23d33696c
Łukasz Kalita <lukkalita@onet.pl>
2025-01-13 19:47:30 +0100

    Sorcerer build

--- src/main/java/org/example/character/characterClasses/Sorcerer.java
+++ src/main/java/org/example/character/characterClasses/Sorcerer.java
@@ -8,5 +8,25 @@ import org.example.enums.TypeOfWeapons;
 import java.util.List;
 
 public class Sorcerer extends CharacterClass {
-
+    public Sorcerer(String name,
+                    boolean isCore,
+                    List<TypeOfWeapons> allowedWeaponList,
+                    List<TypeOfArmor> allowedArmorList,
+                    List<Checks> allowedChecks,
+                    int level,
+                    SpellCastSystem spellCastSystem) {
+        super("Sorcerer",
+                true,
+                List.of(TypeOfWeapons.NONE),
+                List.of(TypeOfArmor.CLOTH),
+                List.of(Checks.SPELL_MAINTENANCE,
+                        Checks.SPELL_USE,
+                        Checks.MONSTER_KNOWLEDGE,
+                        Checks.GENERAL_KNOWLEDGE,
+                        Checks.RESEARCH,
+                        Checks.MAGIC_KNOWLEDGE,
+                        Checks.MEDITATION),
+                1,
+                SpellCastSystem.WORDS_OF_TRUE_POWER);
+    }
 }
