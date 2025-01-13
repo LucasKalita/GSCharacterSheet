89910034d7a0289522b48df5d6fcb7644533446a
Łukasz Kalita <lukkalita@onet.pl>
2025-01-13 19:41:51 +0100

    Shaman build

--- src/main/java/org/example/character/characterClasses/Shaman.java
+++ src/main/java/org/example/character/characterClasses/Shaman.java
@@ -8,5 +8,24 @@ import org.example.enums.TypeOfWeapons;
 import java.util.List;
 
 public class Shaman extends CharacterClass{
-
+    public Shaman(String name,
+                  boolean isCore,
+                  List<TypeOfWeapons> allowedWeaponList,
+                  List<TypeOfArmor> allowedArmorList,
+                  List<Checks> allowedChecks,
+                  int level,
+                  SpellCastSystem spellCastSystem) {
+        super("Shaman",
+                true,
+                List.of(TypeOfWeapons.NONE),
+                List.of(TypeOfArmor.CLOTH,
+                        TypeOfArmor.LIGHT),
+                List.of(Checks.SPELL_MAINTENANCE,
+                        Checks.SPELL_USE,
+                        Checks.MONSTER_KNOWLEDGE,
+                        Checks.SIX_SENSE,
+                        Checks.MEDITATION),
+                1,
+                SpellCastSystem.SPIRIT_ARTS);
+    }
 }
