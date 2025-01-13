a9d28d9bca8e8f1337dfb5687c7e02ed3531394f
Łukasz Kalita <lukkalita@onet.pl>
2025-01-13 19:26:41 +0100

    priest build

--- src/main/java/org/example/character/characterClasses/Priest.java
+++ src/main/java/org/example/character/characterClasses/Priest.java
@@ -8,5 +8,26 @@ import org.example.enums.TypeOfWeapons;
 import java.util.List;
 
 public class Priest extends CharacterClass{
+    public Priest(String name,
+                  boolean isCore,
+                  List<TypeOfWeapons> allowedWeaponList,
+                  List<TypeOfArmor> allowedArmorList,
+                  List<Checks> allowedChecks,
+                  int level,
+                  SpellCastSystem spellCastSystem) {
+        super("Priest",
+                true,
+                List.of(TypeOfWeapons.NONE),
+                List.of(TypeOfArmor.CLOTH),
+                List.of(Checks.SPELL_MAINTENANCE,
+                        Checks.SPELL_USE,
+                        Checks.FIRST_AID,
+                        Checks.MONSTER_KNOWLEDGE,
+                        Checks.WORSHIP,
+                        Checks.THEOLOGY
+                        ),
+               1,
+                SpellCastSystem.MIRACLES);
 
+    }
 }
