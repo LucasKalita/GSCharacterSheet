8184ede7c3ff2d47c55b01ae1d59a7e61f1b3e14
Łukasz Kalita <lukkalita@onet.pl>
2025-01-13 18:33:20 +0100

    Class for Fighter

--- src/main/java/org/example/character/characterClasses/Fighter.java
+++ src/main/java/org/example/character/characterClasses/Fighter.java
@@ -9,7 +9,6 @@ import java.util.List;
 
 public class Fighter extends CharacterClass {
 
-
     public Fighter(String name,
                    boolean isCore,
                    List<TypeOfWeapons> allowedWeaponList,
@@ -17,6 +16,7 @@ public class Fighter extends CharacterClass {
                    List<Checks> allowedChecks,
                    SpellCastSystem spellCastSystem) {
         super("Fighter", true, List.of(TypeOfWeapons.values()), List.of(TypeOfArmor.values()),
-                List.of(Checks.DODGE, Checks.HIT, Checks.BLOCK), 0, SpellCastSystem.NONE);
+                List.of(Checks.DODGE, Checks.HIT, Checks.BLOCK), 1, SpellCastSystem.NONE);
     }
+
 }
